f957061c736dc01446724789aad5a400f673751e
Kamel Gerado <kamel1419@gmail.com>
2021-09-21 03:20:27 +0300

    first release

--- maqam/analyzer/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import QuranFile
-
-# Register your models here.
-admin.site.register(QuranFile)
--- maqam/analyzer/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class QuranFile(models.Model):
-
-    quran_file = models.FileField()
-
-    def __str__(self):
-        return f'file {self.quran_file}'
